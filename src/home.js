ca80e9ede6c2faea1a49d523a35a376ff568ace3
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-03-28 14:47:44 +0530

    Adding styling to home page

--- src/home.js
+++ src/home.js
@@ -5,12 +5,17 @@ function createHome(){
     const restaurantDescription = document.createElement("div");
     restaurantDescription.innerHTML = `
     <h1 class="name">Restaurant Name</h1>
-    <h1>Authentic, Fresh and delicious food delivered to your doorstep</h1>
+    <h1>Savor the authentic tastes of India, crafted with love and served with a side of warmth and hospitality.</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe eligendi labore quod quam laboriosam vero cupiditate .</p>
     <button>Reservation</button>`;
 
-    const restaurantImg = document.createElement("img");
-    restaurantImg.src = image;
+
+
+    const restaurantImg = document.createElement("div");
+    const img = new Image();
+    img.src = "https://statics.vinpearl.com/Indian-Restaurants-In-Phu-Quoc-01_1702657085.jpg";
+    
+    restaurantImg.append(img);
 
     homeDiv.append(restaurantDescription,restaurantImg);
     
